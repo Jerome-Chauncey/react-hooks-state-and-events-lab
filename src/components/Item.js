a314dcd74df73a7111b34530877625eddf151147
Jerome-Chauncey <jerome.chauncey@student.moringaschool.com>
2025-04-11 01:56:59 +0300

    works with multiple state variables together

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleCartToggle(){
+    setInCart((prevInCart) => !prevInCart);
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick= {handleCartToggle} className="add">Add to Cart</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selected, setSelected] = useState("");
+
+  function handleFilterChange(event) {
+    setSelected(event.target.value);
+  }
+
+  const filteredItems = items.filter((item) =>{
+    if (selected === "") return true; 
+    return item.category === selected;
+});
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
+      <div className="Filter"> 
+        <select name="filter" value= {selected} onChange={handleFilterChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,8 +23,8 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {filteredItems.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} /> 
         ))}
       </ul>
     </div>
